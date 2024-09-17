9261d6b5be60c286b45762a94d1e6f7dfac71b3b
Manjot Singh Oberoi <78747188+ken1000minus7@users.noreply.github.com>
2024-09-17 15:44:37 +0530

    Sedness

---
+++ src/test/java/ut/CalculatorTest.java
@@ -0,0 +1,50 @@
+package ut;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.example.Calculator;
+
+public class CalculatorTest {
+
+	static Calculator cl;
+
+	@BeforeClass
+	public static void setUp() {
+		cl = new Calculator();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.gc();
+	}
+
+	@Test
+	public void testAdd() {
+
+        assertEquals(3, cl.doAdd(1, 2), 0.0);
+	}
+
+	@Test
+	public void testSub() {
+        assertEquals(0, cl.doSub(1, 1), 0.0);
+	}
+
+	@Test
+	public void testMul() {
+        assertEquals(18, cl.doMul(9, 2), 0.0);
+	}
+
+	@Test
+	public void testDiv() {
+        assertEquals(4, cl.doDiv(8, 2), 0.0);
+	}
+
+	@Test
+	public void testDivZero() {
+		assertEquals(Double.POSITIVE_INFINITY, cl.doDiv(10, 0), 0.0);
+	}
+}
